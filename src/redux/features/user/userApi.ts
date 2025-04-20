257cc243f78119c453dbd7107cee5af67d9cb0f0
developer-shourav <mrinmoyraj823@gmail.com>
2025-04-21 02:45:33 +0600

    User API Tags added

--- src/redux/features/user/userApi.ts
+++ src/redux/features/user/userApi.ts
@@ -8,29 +8,39 @@ const userApi = baseApi.injectEndpoints({
                 method: "PATCH",
                 body: updateInfo,
             }),
+            invalidatesTags: ["Users"],
         }),
+
         userChangePassword: builder.mutation({
             query: (passwordInfo) => ({
                 url: "/users/change-password",
                 method: "PATCH",
                 body: passwordInfo,
             }),
+            invalidatesTags: ["Users"],
         }),
+
         getAllUsers: builder.query({
             query: () => ({
                 url: `/users`,
                 method: "GET",
             }),
+            providesTags: ["Users"],
         }),
+
         getSingleUser: builder.query({
             query: (userId) => ({
                 url: `/users/${userId}`,
                 method: "GET",
             }),
+            providesTags: ["Users"],
         }),
     }),
 });
 
 export const {
-    useUpdateUserInfoMutation, useUserChangePasswordMutation, useGetAllUsersQuery, useGetSingleUserQuery
+    useUpdateUserInfoMutation,
+    useUserChangePasswordMutation,
+    useGetAllUsersQuery,
+    useGetSingleUserQuery,
 } = userApi;
