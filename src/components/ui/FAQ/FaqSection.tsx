94b89d292fe47ca05802198fdea06c3d48956a71
developer-shourav <mrinmoyraj823@gmail.com>
2025-07-17 13:02:17 +0600

    faq secion's sizing issue solved

--- src/components/ui/FAQ/FaqSection.tsx
+++ src/components/ui/FAQ/FaqSection.tsx
@@ -2,8 +2,9 @@ import { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { Plus, Minus } from "lucide-react";
 import { cn } from "@/lib/utils";
-import carGroup from '@/assets/carGroup.png';
+import carGroup from "@/assets/carGroup.png";
 import { WebsiteHeading } from "../WebsiteHeading/WebsiteHeading";
+import SectionWrapper from "../wrapper/SectionWrapper";
 const faqs = [
   {
     question: "What is CarSure?",
@@ -46,55 +47,57 @@ export default function FaqSection() {
 
   return (
     <section className="w-full mx-auto py-8 lg:py-10  xl:py-12">
-      <WebsiteHeading title="Frequently Asked Questions" />
-      <div className="px-3 w-full mx-auto flex justify-center">
-        <img src={carGroup}  loading="lazy" alt="" />
-      </div>
-      <div className=" w-11/12 md:w-10/12 mx-auto lg:w-8/12 space-y-4">
-        {faqs.map((faq, index) => {
-          const isOpen = openIndex === index;
-          return (
-            <div
-              key={index}
-              className={cn(
-                "rounded-xl border   bg-white dark:bg-zinc-900 hover:border-red-500 border-l-4  dark:hover:border-red-500 transition-all overflow-hidden",
-                isOpen
-                  ? "border-red-500"
-                  : "border-gray-200 dark:border-gray-700"
-              )}
-            >
-              <button
-                onClick={() => toggle(index)}
-                className="w-full flex justify-between items-center p-4  text-left text-gray-800 dark:text-gray-100  border-transparent "
+      <SectionWrapper>
+        <WebsiteHeading title="Frequently Asked Questions" />
+        <div className="px-3 w-full mx-auto flex justify-center">
+          <img src={carGroup} loading="lazy" alt="" />
+        </div>
+        <div className=" w-11/12 md:w-10/12 mx-auto lg:w-9/12 space-y-4">
+          {faqs.map((faq, index) => {
+            const isOpen = openIndex === index;
+            return (
+              <div
+                key={index}
+                className={cn(
+                  "rounded-xl border   bg-white dark:bg-zinc-900 hover:border-red-500 border-l-4  dark:hover:border-red-500 transition-all overflow-hidden",
+                  isOpen
+                    ? "border-red-500"
+                    : "border-gray-200 dark:border-gray-700"
+                )}
               >
-                <span className="text-base font-medium">{faq.question}</span>
-                {isOpen ? (
-                  <Minus className="w-5 h-5 text-red-500" />
-                ) : (
-                  <Plus className="w-5 h-5 text-red-500" />
-                )}
-              </button>
+                <button
+                  onClick={() => toggle(index)}
+                  className="w-full flex justify-between items-center p-4  text-left text-gray-800 dark:text-gray-100  border-transparent "
+                >
+                  <span className="text-base font-medium">{faq.question}</span>
+                  {isOpen ? (
+                    <Minus className="w-5 h-5 text-red-500" />
+                  ) : (
+                    <Plus className="w-5 h-5 text-red-500" />
+                  )}
+                </button>
 
-              <AnimatePresence>
-                {isOpen && (
-                  <motion.div
-                    key="content"
-                    initial={{ height: 0, opacity: 0 }}
-                    animate={{ height: "auto", opacity: 1 }}
-                    exit={{ height: 0, opacity: 0 }}
-                    transition={{ duration: 0.3, ease: "easeInOut" }}
-                    className="overflow-hidden"
-                  >
-                    <div className="px-4 pb-4 text-sm text-gray-600 dark:text-gray-300">
-                      {faq.answer}
-                    </div>
-                  </motion.div>
-                )}
-              </AnimatePresence>
-            </div>
-          );
-        })}
-      </div>
+                <AnimatePresence>
+                  {isOpen && (
+                    <motion.div
+                      key="content"
+                      initial={{ height: 0, opacity: 0 }}
+                      animate={{ height: "auto", opacity: 1 }}
+                      exit={{ height: 0, opacity: 0 }}
+                      transition={{ duration: 0.3, ease: "easeInOut" }}
+                      className="overflow-hidden"
+                    >
+                      <div className="px-4 pb-4 text-sm text-gray-600 dark:text-gray-300">
+                        {faq.answer}
+                      </div>
+                    </motion.div>
+                  )}
+                </AnimatePresence>
+              </div>
+            );
+          })}
+        </div>
+      </SectionWrapper>
     </section>
   );
 }
