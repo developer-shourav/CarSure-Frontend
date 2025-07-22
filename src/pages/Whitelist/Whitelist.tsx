fdf82fcfc1bb1500d254e7f029d57a28c33682b4
developer-shourav <mrinmoyraj823@gmail.com>
2025-07-22 14:51:28 +0600

    Favourite page redesinged

--- src/pages/Whitelist/Whitelist.tsx
+++ src/pages/Whitelist/Whitelist.tsx
@@ -28,38 +28,44 @@ export default function WhitelistPage() {
   };
 
   return (
-    <div className="max-w-5xl mx-auto py-10 px-4 mt-[62px] lg:mt-[116px]">
-      <h2 className="text-3xl font-bold mb-6">Your Whitelist</h2>
+    <div className="max-w-5xl mx-auto py-10 px-4 mt-[62px] lg:mt-[116px] min-h-[80vh]">
+      <h2 className="text-2xl sm:text-3xl font-bold mb-6">Your Whitelist</h2>
 
       {whitelist.length === 0 ? (
-        <p>Your whitelist is empty.</p>
+        <p className="text-gray-600">Your whitelist is empty.</p>
       ) : (
-        <div className="space-y-6">
+        <div className="space-y-4">
           {whitelist.map((item) => (
-            <div key={item.id} className="flex items-center justify-between">
-              <div className="flex items-center gap-4">
+            <div
+              key={item.id}
+              className="flex flex-row  justify-between md:items-center gap-2 border p-2 rounded-lg shadow-sm"
+            >
+              <div className="flex items-start gap-2 w-8/12 ">
                 <img
                   src={item.image}
                   className="h-20 w-20 object-cover rounded"
                   alt={item.name}
                 />
                 <div>
-                  <p className="font-semibold">{item.name}</p>
-                  <p>${item.price}</p>
+                  <p className="font-semibold text-base sm:text-lg">
+                    {item.name}
+                  </p>
+                  <p className="text-sm text-gray-600">${item.price}</p>
                 </div>
               </div>
-              <div>
+
+              <div className="flex  flex-col md:flex-row md:justify-end gap-2 w-4/12">
                 <Button
                   onClick={() => handleAddToCart(item)}
-                  className=" me-2"
                   size="sm"
+                  className=""
                 >
                   Add to Cart
                 </Button>
                 <Button
                   onClick={() => handleRemoveFromWhitelist(item.id)}
-                  className="bg-red-600 hover:bg-red-700 text-white"
                   size="sm"
+                  className=" bg-red-600 hover:bg-red-700 text-white"
                 >
                   Remove
                 </Button>
